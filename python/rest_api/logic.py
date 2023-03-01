ab21784dba81985356f75485cbe338d4c4a0720c
Laurent Michel <laurent.michel@astro.unistra.fr>
2023-03-01 15:30:12 +0100

    refactoring

--- python/rest_api/logic.py
+++ python/rest_api/logic.py
@@ -3,13 +3,29 @@ Created on 28 févr. 2023
 
 @author: michel
 '''
-import os
 import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 from astropy.time import Time
 
-from core.STONKS_PreComputed_Position_alert import transient_alert
+from core.STONKS_PreComputed_Position_alert import transient_alert, dec
+
+class ParamHolder:
+    """
+    structure carrying all source parameter in one shot
+    """
+    def __init__(self):
+        self.ra = None
+        self.dec = None
+        self.pos_err = None
+        self.flux = None
+        self.flux_err = None
+        self.band_flux = None
+        self.band_fluxerr = None
+        self.src_num = None
+        self.pn_offax = None
+        self.m1_offax = None
+        self.m2_offax = None 
 
 def process_one_observation(obsmli_path): 
     print(f"Loading EPIC source list {obsmli_path}")
@@ -35,11 +51,23 @@ def process_one_observation(obsmli_path):
             zip(sources_raw["RA"],sources_raw["DEC"],sources_raw["RADEC_ERR"], sources_raw["EP_TOT_FLUX"],\
                     sources_raw["ERR_EP_TOT_FLUX"],tab_band_fluxes, tab_band_fluxerr, sources_raw["SRC_NUM"],
                 sources_raw["PN_OFFAX"],sources_raw["M1_OFFAX"],sources_raw["M2_OFFAX"]):
+        param_holder = ParamHolder()
+        param_holder.ra = ra 
+        param_holder.dec = dec
+        param_holder.pos_err = pos_err
+        param_holder.flux = flux
+        param_holder.flux_err = flux_err
+        param_holder.band_flux = band_flux
+        param_holder.band_fluxerr = band_fluxerr
+        param_holder.src_num = src_num
+        param_holder.pn_offax = pn_offax
+        param_holder.m1_offax = m1_offax
+        param_holder.m2_offax = m2_offax
         print (f"Processing source {src_num}")
-        process_one_source(ra, dec, pos_err, flux, flux_err, band_flux, band_fluxerr, src_num, pn_offax, m1_offax, m2_offax, dict_observation_metadata)          
+        process_one_source(param_holder, dict_observation_metadata)          
         
          
-def process_one_source(ra, dec, pos_err, flux, flux_err, band_flux, band_fluxerr, src_num, pn_offax, m1_offax, m2_offax, observation_metadata):
+def process_one_source(param_holder, observation_metadata):
     tab_alerts=[]
     tab_dic_infos = []
 
@@ -47,14 +75,22 @@ def process_one_source(ra, dec, pos_err, flux, flux_err, band_flux, band_fluxerr
     dict_detection_info["ObsID"]=observation_metadata["ObsID"]
     dict_detection_info["Date Obs"]=observation_metadata["DateObs"]
     dict_detection_info["Target Name"]=observation_metadata["TargetName"]
-    dict_detection_info['SRCNUM'] = str(src_num)
-    dict_detection_info['Off-axis Angles'] = f'PN: {pn_offax:.1f}", M1: {m1_offax:.1f}", M2: {m2_offax:.1f}"'
-    dict_detection_info['Source RA']=np.round(ra, 4)
-    dict_detection_info['Source Dec']=np.round(dec, 4)
-    dict_detection_info['Position Error']=f'{pos_err:.2f}"'
+    dict_detection_info['SRCNUM'] = str(param_holder.src_num)
+    dict_detection_info['Off-axis Angles'] = f'PN: {param_holder.pn_offax:.1f}", M1: {param_holder.m1_offax:.1f}", M2: {param_holder.m2_offax:.1f}"'
+    dict_detection_info['Source RA']=np.round(param_holder.ra, 4)
+    dict_detection_info['Source Dec']=np.round(param_holder.dec, 4)
+    dict_detection_info['Position Error']=f'{param_holder.pos_err:.2f}"'
 
-    result_alert = transient_alert(1, ra, dec, pos_err, flux, flux_err, band_flux,
-                                 band_fluxerr, observation_metadata["MJD"], var_flag=False)
+    result_alert = transient_alert(1,
+                                   param_holder.ra,
+                                   param_holder.dec,
+                                   param_holder.pos_err,
+                                   param_holder.flux,
+                                   param_holder.flux_err,
+                                   param_holder.band_flux,
+                                   param_holder.band_fluxerr,
+                                   observation_metadata["MJD"],
+                                   var_flag=False)
     if result_alert!=[]: #If there is a transient alert
         tab_alerts += result_alert
         tab_dic_infos.append(dict_detection_info)
