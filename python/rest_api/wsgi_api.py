0079a3095201b4824931b8831c53413f53b8f3e5
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2023-12-18 16:44:50 +0100

    why cache cleaning does not start?

--- python/rest_api/wsgi_api.py
+++ python/rest_api/wsgi_api.py
@@ -9,8 +9,9 @@ Must be run with gunicorn: ``gunicorn --bind 0.0.0.0:5000 api.wsgi_api:applicati
 from rest_api.flask_app import app as application
 from cache import Cache
 
+print("starting cache monitoring")
+Cache.running = True
+Cache().start()    
+
 if __name__ == "__main__":
-    print("starting cache monitoring")
-    Cache.running = True
-    Cache().start()    
     application.run()
