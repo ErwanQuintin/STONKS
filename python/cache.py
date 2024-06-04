0008b6c4f19f96ae12af70698293543f881da373
MICHEL LAURENT <laurent.michel@serendib.astro.unistra.fr>
2024-06-04 09:52:16 +0200

    cache tuning

--- python/cache.py
+++ python/cache.py
@@ -27,7 +27,7 @@ class Cache(Thread):
         # loop over all the files 
         for ftr in files:
             file_time = os.stat(ftr).st_mtime
-            dt = current_time - 10*day
+            dt = current_time - 2*day
             if(file_time < dt):
                 obs = re.search("^.*([0-9]{10}).*$", ftr).group(1)
                 files = glob.glob(cache_dir + f'/*{obs}*', recursive=False)    
@@ -49,7 +49,7 @@ class Cache(Thread):
         # loop over all the files 
         for session in files: 
             file_time = os.stat(session).st_mtime 
-            dt = current_time - 10*day
+            dt = current_time - 2*day
             if(file_time < dt): 
                 print(f" Delete : {session}  dt={(current_time - file_time)} ") 
                 if os.path.isdir(session):
